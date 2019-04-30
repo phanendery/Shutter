17b0ecefdc5f585d259d9013c63e0c5a4d3541a6
phanendery <phanendery@gmail.com>
2019-04-30 16:23:03 -0700

    Added User migration and User model

---
+++ frontend/shutter.jsx
@@ -0,0 +1,9 @@
+// frontend/shutter.jsx
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Welcome to Shutter</h1>, root);
+});
