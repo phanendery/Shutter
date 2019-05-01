63543e6a881ffb263afda3298cf9b00657b65e72
phanendery <phanendery@gmail.com>
2019-04-30 18:47:55 -0700

    Working Login, but need to edit email requirement

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import GreetingContainer from "./greeting/greeting_container";
 import { Route } from "react-router-dom";
 
+import LoginFormContainer from "./session_form/login_form_container";
+import SignupFormContainer from "./session_form/signup_form_container";
+
 const App = () => (
   <div>
     <header>
-      <h1>Shutter</h1>
       <GreetingContainer />
     </header>
 
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -38,7 +38,7 @@ class SessionForm extends React.Component {
     return (
       <div className="login-form-container">
         <form onSubmit={this.handleSubmit} className="login-form-box">
-          Welcome to Shutter!
+          <h1>Welcome to Shutter!</h1>
           <br />
           Please {this.props.formType} or {this.props.navLink}
           {this.renderErrors()}
@@ -75,3 +75,5 @@ class SessionForm extends React.Component {
     );
   }
 }
+
+export default SessionForm;
