d399fc64b930a862cc25df2ff605cd1dceee56d3
phanendery <phanendery@gmail.com>
2019-05-09 09:32:05 -0700

    working upload modal

--- frontend/components/pictures/picture_upload/picture_upload.jsx
+++ frontend/components/pictures/picture_upload/picture_upload.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import Form from "../../form/form";
+import Modal from "../../modal";
 
 class PictureUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
+  showModal() {
+    this.setState({ showModal: true });
+  }
+
+  hideModal() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     const { postPicture, currentUser } = this.props;
     return (
       <div>
-        <Form postPicture={postPicture} currentUser={currentUser} />
+        <button onClick={this.showModal}>Upload</button>
+        <Modal handleClose={this.hideModal} show={this.state.showModal}>
+          <div>
+            <Form postPicture={postPicture} currentUser={currentUser} />
+          </div>
+        </Modal>
       </div>
     );
   }
