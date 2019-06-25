1292c8af90cd601fe27bdcab8d77e6a0dcbf74d4
phanendery <phanendery@gmail.com>
2019-06-25 11:21:55 -0700

    fixed like associations

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,4 +2,5 @@ class Picture < ApplicationRecord
     validates :pic_name, :user_id, presence: true
     belongs_to :user
     has_one_attached :photo
+    has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   after_initialize :ensure_session_token
 
   has_many :pictures
+  has_many :likes
+  
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
