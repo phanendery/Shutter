816697d00a3593eb745f8c5bbcc89231064df5f4
phanendery <phanendery@gmail.com>
2019-08-02 10:37:56 -0700

    fixing slow load issue by using Cloudinary:

--- app/views/api/pictures/index.json.jbuilder
+++ app/views/api/pictures/index.json.jbuilder
@@ -2,6 +2,7 @@
     json.set! picture.id do
         json.extract! picture, :id, :pic_name, :user_id
         json.photoUrl url_for(picture.photo)
+        json.serviceUrl picture.photo.service_url
     end
       
 end
--- frontend/components/pictures/picture_index_item.jsx
+++ frontend/components/pictures/picture_index_item.jsx
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 const PictureIndexItem = props => {
   return (
     <React.Fragment>
-      <img src={props.picture.photoUrl} />
+      <img
+        src={`https://res.cloudinary.com/dfeo7demm/image/fetch/w_500,h_500,c_fit/${
+          props.picture.serviceUrl
+        }`}
+      />
     </React.Fragment>
   );
 };
